daa5b16eeb0a8f61ec946a07b36cc4ca756645d2
Alexandre <webmaster@alexandre.re>
2024-03-04 13:40:08 +0400

    feat: remove unused import

--- src/main/java/com/example/springboottodoapiserver/controllers/TodoItemController.java
+++ src/main/java/com/example/springboottodoapiserver/controllers/TodoItemController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
